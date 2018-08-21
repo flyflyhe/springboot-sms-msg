527af234097796ffbc1cb2d0181dd4f34572ddc9
hejinxue001 <hejinxue001@163.com>
2018-08-21 15:50:26 +0800

    双重检查锁

--- src/main/java/sms/connect/MysqlConnection.java
+++ src/main/java/sms/connect/MysqlConnection.java
@@ -11,15 +11,20 @@ import java.io.InputStream;
 @Component
 public class MysqlConnection {
     private static volatile SqlSessionFactory sqlSessionFactory;
-
+    
+    //双重检查锁 保证单例唯一
     public  SqlSessionFactory getMybatisSqlSessionFactory()
             throws Exception {
-
+        
+        //防止每次进入都执行同步代码块
         if (sqlSessionFactory == null) {
             synchronized(this){
-                String resource = "mybatis-config.xml";
-                InputStream inputStream = Resources.getResourceAsStream(resource);
-                sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+                //防止多次创建实例
+                if (sqlSessionFactory == null) {
+                    String resource = "mybatis-config.xml";
+                    InputStream inputStream = Resources.getResourceAsStream(resource);
+                    sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+                } 
             }
         }
 
